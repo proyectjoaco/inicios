de9bd677d76fc0323dbde05af83819a6d95be380
proyectjoaco <joacoar1830@gmail.com>
2025-12-18 16:43:07 -0300

    Agrego ejercicios de listas, tuplas y bucles

---
+++ 05_tupple.py
@@ -0,0 +1,23 @@
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (18, 1.80, "joaquin", "aranda", 2007, "aranda")
+my_other_tuple = (1, 2, 3, 4, 5)
+print (my_tuple)
+print (type(my_tuple))
+print (my_tuple[0])
+print (my_tuple[4])
+print (type(my_tuple[1]))#se puede ver el "type", de un elemento dentro de una tupla o lista
+print (type(my_tuple[3]))#se puede ver el "type", de un elemento dentro de una tupla o lista
+print (len(my_tuple))#lee cantidad de elementos de la tuple
+print (len(my_tuple[3]))#lee la cantidad de caracteres del elemento seleccionado de la tuple
+print (my_tuple.index("aranda"))#te dice la posicion del elemento seleccionado (si se repite el elemento, solo pone el primero)
+print (type(my_other_tuple))
+
+
+print (type (my_tuple + my_other_tuple))
+my_plus_tuple = my_tuple + my_other_tuple#no se puede modificar las tuples pero si se puede sumarlas para una nueva
+print (type (my_plus_tuple))
+print (my_plus_tuple[3:6])
+#del my_plus_tuple
+print (my_plus_tuple)
---
+++ 06_practica.py
@@ -0,0 +1,70 @@
+### le pedi a chat gpt un ejecicio que contenga los 5 puntos que aprendi Enunciado: ###
+"""
+-Pedí al usuario:
++nombre
++edad
++altura
+-Creá una tupla llamada usuario con esos datos en este orden:
++(nombre, edad, altura)
+-Guardá esa tupla dentro de una lista llamada usuarios.
+-Mostrá por pantalla:
++la lista completa
++cuántos usuarios hay registrados
++el nombre del primer usuario
++el último dato del usuario (usando índice negativo)
+-Usá operadores para:
++sumar 1 a la edad del usuario
++mostrar un mensaje indicando si es mayor de edad (>= 18)
+"""
+"""
+name = (input("cual es tu nombre?: "))
+age = (input("cuantos años tienes?: "))
+height = (input("cuanto mides?: "))
+user1_tuple = name, age, height
+print (user1_tuple)
+list_users = list()
+list_users = [user1_tuple]
+print (list_users)
+print(len(list_users))
+print (name)
+print (user1_tuple[-1])
+age = (int(age) + 1)
+print (age)
+if age >= 18:
+
+    print ("eres mayor de edad, puedes pasar")
+
+else:
+
+    print ("eres menor de edad, no puedes pasar")
+    """
+### segundo ejercicio###
+"""
+Ejercicio puente (muy corto)
+
+Hacelo en 10–15 minutos, no más.
+
+Enunciado
+
+-Pedí nombres de usuarios en un loop
+
+-Guardalos en una lista
+
+-Permití salir escribiendo "salir"
+
+-Mostrá la lista final
+
+Nada de archivos. Nada de JSON. Simple.
+"""
+nombres_users = []
+
+while True:
+    name = input("ingrese un nombre (o salir): ")
+
+    if name == "salir":
+        print("los usuarios son:", nombres_users)
+        break
+    else:
+        nombres_users.append(name)
+
+
